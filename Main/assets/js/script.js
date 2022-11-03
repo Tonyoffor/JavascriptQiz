063f414cde2fd5cea0c65aea6dc413e1ca5416d3
Tonyoffor <tonyc.offor@gmail.com>
2022-11-03 09:31:55 -0400

    decided to change the layout; brought my questions into one js file; redesigned my index.html but not added style yet

--- Main/assets/js/script.js
+++ Main/assets/js/script.js
@@ -1,16 +1,75 @@
-var wordBlank = document.querySelector(".word-blanks");
-var win = document.querySelector(".win");
-var lose = document.querySelector(".lose");
-var timerElement = document.querySelector(".timer-count");
-var startButton = document.querySelector(".start-button");
+var header = document.querySelector(".header");
+var startbutton = document.querySelector(".startbutton");
+var stopbutton = document.querySelectorAll("stopbutton")
 
-var chosenWord = "";
-var numBlanks = 0;
-var winCounter = 0;
-var loseCounter = 0;
-var isWin = false;
-var timer;
-var timerCount;
+var QuizQuestions = [
+  {
+  numb: 1,
+  question: "Where in the index.html should you put your script.js tag?",
+  ans: "At the bottom of the code before the closing </body> tag ", 
+  options: [
+    "At the bottom of the code before the closing </body> tag ", 
+    "It does not matter you can put it anywhere",
+    "At the top of your code before the </head> tag",
+    "In your </header> tag"
+  ]
+  },
+  {
+    numb: 2,
+  question: "Where in the index.html should you put your style.css tag?",
+  ans: "At the top of your code before the </head> tag",
+  options: [
+    "It does not matter you can put it anywhere",
+    "In your </header> tag",
+    "At the top of your code before the </head> tag",
+    "At the bottom of the code before the closing </body> tag "  
+  ]
+  },
+  {
+    numb: 3,
+  question: "What does the Media querie function do?",
+  ans: "It helps adjust the page to different screen sizes",
+  options: [
+    "It helps adjust the page to different screen sizes",
+    "It helps show what is hiden in on a page",
+    "It doesn't do anything",
+    "It is used call a for loop"
+  ]
+  },
+  {
+    numb: 4,
+  question: "In style.css how do you target an ID element?",
+  ans: "With the # symbol",
+  options: [
+    "With the # symbol",
+    "With the . symbol",
+    "With the ! symbol",
+    "With the & symbol"
+  ]
+  },
+  {
+    numb: 5,
+  question: "In style.css how do you target an class element?",
+  ans: "With the . symbol",
+  options: [
+    "With the . symbol",
+    "With the ^ symbol",
+    "With the $ symbol",
+    "With the % symbol",
+  ]
+  },
+  ]
+  
+
+
+
+
+
+
+
+
+
+
 
 // Arrays used to create blanks and letters on screen
 var lettersInChosenWord = [];
