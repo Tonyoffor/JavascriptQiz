df3b7e8904fc0f1f8e0a700deb176798d36af33a
Tonyoffor <tonyc.offor@gmail.com>
2022-11-04 09:10:02 -0400

    made some edits to the event listener and other areas of the code

--- Main/assets/js/script.js
+++ Main/assets/js/script.js
@@ -2,26 +2,30 @@ var startGame = document.querySelector("#startGame");
 var reset = document.querySelector("#reset");
 var timer = document.querySelector("#timer");
 var counter = document.querySelector("#counter");
-var questionArea = document.querySelector(".questionArea")
-var answerArea = document.querySelector(".answerArea")
-
+var questionArea = document.querySelector(".questionArea");
+var answerArea = document.querySelector(".answerArea");
+var Score = document.querySelector("#Score")
+var resetButton = document.querySelector("#resetButton")
+var startButton = document.querySelector("#startbutton")
 
 var timer = 0;
 var timerleft = 80;
+var timerCount;
+var Userchoice;
+var interval;
+var ans;
 
 var QuizQuestions = [
   {
-  numb: 1,
+    numb: 1,
   question: "Where in the index.html should you put your script.js tag?",
   ans: "At the bottom of the code before the closing </body> tag ", 
   options: [
-    "At the bottom of the code before the closing </body> tag ", 
+     "At the bottom of the code before the closing </body> tag ", 
     "It does not matter you can put it anywhere",
-    "At the top of your code before the </head> tag",
+    "At the top of your code",
     "In your </header> tag"
-  ]
-  },
-  {
+],
     numb: 2,
   question: "Where in the index.html should you put your style.css tag?",
   ans: "At the top of your code before the </head> tag",
@@ -29,7 +33,7 @@ var QuizQuestions = [
     "It does not matter you can put it anywhere",
     "In your </header> tag",
     "At the top of your code before the </head> tag",
-    "At the bottom of the code before the closing </body> tag "  
+    "At the bottom of the code"  
   ]
   },
   {
@@ -49,7 +53,7 @@ var QuizQuestions = [
   ans: "With the # symbol",
   options: [
     "With the # symbol",
-    "With the . symbol",
+    "With the @ symbol",
     "With the ! symbol",
     "With the & symbol"
   ]
@@ -59,7 +63,7 @@ var QuizQuestions = [
   question: "In style.css how do you target an class element?",
   ans: "With the . symbol",
   options: [
-    "With the . symbol",
+    "With the & symbol",
     "With the ^ symbol",
     "With the $ symbol",
     "With the % symbol",
@@ -67,190 +71,42 @@ var QuizQuestions = [
   },
   ]
   
+  var ans = [
+    "At the bottom of the code before the closing </body> tag ", 
+    "At the top of your code before the </head> tag",
+    "It helps adjust the page to different screen sizes",
+    "With the # symbol",
+    "With the . symbol",
+  ];
+  
+
+  function startTimer(){
+//the start button will start this prompt 
+  };
 
 
+function reset(){
+ // Resets timer, game and go back to the 1t question.
+ timer = 0;
+ timerleft = 0;
+ startGame = QuizQuestions;
+ // Renders win and loss counts and sets them into client storage
 
-
-
-
-
-
-
-
-
-// Arrays used to create blanks and letters on screen
-var lettersInChosenWord = [];
-var blanksLetters = [];
-
-// Array of words the user will guess
-var words = ["variable","array", "modulus", "object", "function", "string", "boolean"];
-
-// The init function is called when the page loads 
-function init() {
-  getWins();
-  getlosses();
 }
 
-// The startGame function is called when the start button is clicked
-function startGame() {
-  isWin = false;
-  timerCount = 10;
-  // Prevents start button from being clicked when round is in progress
-  startButton.disabled = true;
-  renderBlanks()
-  startTimer()
+function gameover(){
+
 }
 
-// The winGame function is called when the win condition is met
-function winGame() {
-  wordBlank.textContent = "YOU WON!!!🏆 ";
-  winCounter++
-  startButton.disabled = false;
-  setWins()
-}
+function nextQuestion(){
+  for (let i = 0; i < QuizQuestions.length; i++) {
+    text += QuizQuestions[i] + "";
 
-// The loseGame function is called when timer reaches 0
-function loseGame() {
-  wordBlank.textContent = "GAME OVER";
-  loseCounter++
-  startButton.disabled = false;
-  setLosses()
-}
-
-// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
-function startTimer() {
-  // Sets timer
-  timer = setInterval(function() {
-    timerCount--;
-    timerElement.textContent = timerCount;
-    if (timerCount >= 0) {
-      // Tests if win condition is met
-      if (isWin && timerCount > 0) {
-        // Clears interval and stops timer
-        clearInterval(timer);
-        winGame();
-      }
-    }
-    // Tests if time has run out
-    if (timerCount === 0) {
-      // Clears interval
-      clearInterval(timer);
-      loseGame();
-    }
-  }, 1000);
-}
-
-// Creates blanks on screen
-function renderBlanks() {
-  // Randomly picks word from words array
-  chosenWord = words[Math.floor(Math.random() * words.length)];
-  lettersInChosenWord = chosenWord.split("");
-  numBlanks = lettersInChosenWord.length;
-  blanksLetters = []
-  // Uses loop to push blanks to blankLetters array
-  for (var i = 0; i < numBlanks; i++) {
-    blanksLetters.push("_");
-  }
-  // Converts blankLetters array into a string and renders it on the screen
-  wordBlank.textContent = blanksLetters.join(" ")
-}
-
-// Updates win count on screen and sets win count to client storage
-function setWins() {
-  win.textContent = winCounter;
-  localStorage.setItem("winCount", winCounter);
-}
-
-// Updates lose count on screen and sets lose count to client storage
-function setLosses() {
-  lose.textContent = loseCounter;
-  localStorage.setItem("loseCount", loseCounter);
-}
-
-// These functions are used by init
-function getWins() {
-  // Get stored value from client storage, if it exists
-  var storedWins = localStorage.getItem("winCount");
-  // If stored value doesn't exist, set counter to 0
-  if (storedWins === null) {
-    winCounter = 0;
-  } else {
-    // If a value is retrieved from client storage set the winCounter to that value
-    winCounter = storedWins;
-  }
-  //Render win count to page
-  win.textContent = winCounter;
-}
-
-function getlosses() {
-  var storedLosses = localStorage.getItem("loseCount");
-  if (storedLosses === null) {
-    loseCounter = 0;
-  } else {
-    loseCounter = storedLosses;
-  }
-  lose.textContent = loseCounter;
-}
-
-function checkWin() {
-  // If the word equals the blankLetters array when converted to string, set isWin to true
-  if (chosenWord === blanksLetters.join("")) {
-    // This value is used in the timer function to test if win condition is met
-    isWin = true;
+    // if userchoice !== ans then send score to wrong answer if right then send to correct answer
   }
 }
 
-// Tests if guessed letter is in word and renders it to the screen.
-function checkLetters(letter) {
-  var letterInWord = false;
-  for (var i = 0; i < numBlanks; i++) {
-    if (chosenWord[i] === letter) {
-      letterInWord = true;
-    }
-  }
-  if (letterInWord) {
-    for (var j = 0; j < numBlanks; j++) {
-      if (chosenWord[j] === letter) {
-        blanksLetters[j] = letter;
-      }
-    }
-    wordBlank.textContent = blanksLetters.join(" ");
-  }
-}
 
-// Attach event listener to document to listen for key event
-document.addEventListener("keydown", function(event) {
-  // If the count is zero, exit function
-  if (timerCount === 0) {
-    return;
-  }
-  // Convert all keys to lower case
-  var key = event.key.toLowerCase();
-  var alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
-  // Test if key pushed is letter
-  if (alphabetNumericCharacters.includes(key)) {
-    var letterGuessed = event.key;
-    checkLetters(letterGuessed)
-    checkWin();
-  }
-});
-
-// Attach event listener to start button to call startGame function on click
-startButton.addEventListener("click", startGame);
-
-// Calls init() so that it fires when page opened
-init();
-
-// Bonus: Add reset button
-var resetButton = document.querySelector(".reset-button");
-
-function resetGame() {
-  // Resets win and loss counts
-  winCounter = 0;
-  loseCounter = 0;
-  // Renders win and loss counts and sets them into client storage
-  setWins()
-  setLosses()
-}
 // Attaches event listener to button
-resetButton.addEventListener("click", resetGame);
+resetButton.addEventListener("click", reset);
+startButton.addEventListener("click", startGame);
