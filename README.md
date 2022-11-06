81ce32a3ef428cc27a9db736d8978dc553490f26
Tonyoffor <tonyc.offor@gmail.com>
2022-11-06 17:41:34 -0500

    added comments on the files explaining what most of the code is doing

--- README.md
+++ README.md
@@ -3,28 +3,14 @@ In this challenge I was meant to create a JavaScript Quiz game where the questio
 
 As you start the game a timer start and then the first question of five shows up. 
 
-If you get the 
+If you get the answer wright then you get a point but if you get the question wrong then time is deducted from the clock.
+
+If the timer runs out or the user reaches the last question then the Text Game over, the users score and a text box to write their initials will show up
 
 
-![FinalProduct](/Main/assets/img/FinalProduct.png)
+![FinalProduct](./assets/FinalProduct.png)
 
-Github Repo:
-Deployed Link:
+Github Repo: https://github.com/Tonyoffor/JavascriptQiz
 
+Deployed Link:  https://tonyoffor.github.io/JavascriptQiz/
 
-GIVEN I am taking a code quiz
-
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-
-WHEN I answer a question
-THEN I am presented with another question
-
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-
-WHEN the game is over
-THEN I can save my initials and score
--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
+// Here we defined some of our variables 
 var startGame = document.querySelector("#startGame");
 var reset = document.querySelector("#reset");
 var time_sec = document.getElementById("time_sec");
 var QuizQuestions = document.querySelector(".Quizquestions");
-
+var Initials=document.querySelector("Initials");
 var Score = document.querySelector("#Score");
-var resetButton = document.querySelector("#resetButton");
 var startButton = document.querySelector("#startbutton");
 
 
@@ -18,7 +18,7 @@ var interval;
 var ans;
 
 
-
+//This is the first main function that is meant to start the game
 function gameStart(){
   timerCount = 20;
   console.log("inside start game");
@@ -28,7 +28,7 @@ function gameStart(){
 }
 
 
-
+//This function starts the timer it is called by the gaestart function
  function startTimer(){
   timer = setInterval(function(){
     timerCount--;
@@ -43,24 +43,32 @@ function gameStart(){
   
   }
 
+  //this function is responsible for increasing the users score when they get a question right
 function increasescore(index){
 scorecounter= scorecounter+1;
-max ="10";
 Score.innerHTML="Score: "+scorecounter;
 nextQuestion(index);
 }
 
+//this reduces the timer by 5 points
 function decreasetime(){
   timerCount=timerCount-5;
 }
 
+//this fuction shows the next question when the right answer is selected
 function nextQuestion(index){
 if (index<4){
   document.getElementsByClassName("questions")[index+1].style.display="block";
 }
 }
 
+function show_initials(){
+  if(Initials=document.querySelector("Initials").value){
+    sessionStorage.Initials.value
+  }
+}
 
+//This function displays all the hidden values
 function Gameover(){
   document.getElementById("Gameover").style.display="block";
 document.getElementById("Gameover").innerHTML ="GameOver";
@@ -68,5 +76,6 @@ document.getElementById("initials").style.display="block";
 document.getElementById("Score").style.display="block";
 }
 
+//This is an event listener that calls the first function "start game"
 startButton.addEventListener("click", startGame);
 
